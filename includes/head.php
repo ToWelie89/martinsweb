285a0f91142936b765d77473e512fbbcb8705840
Martin Sonesson <sonesson8909@hotmail.com>
2014-09-08 00:13:39 +0200

    icon testing

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="http://cdn.sstatic.net/stackoverflow/img/favicon.ico">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta charset="utf-8"/>
