bb3403ad198b955406f7dcc5e08d8e9d5f18211d
Martin Sonesson <sonesson8909@hotmail.com>
2015-01-21 22:24:33 +0100

    added 404 error page

---
+++ includes/mainContent/404Content.php
@@ -0,0 +1,11 @@
+<div class="centeringBox">
+    <div class="centeringInnerBox">
+    	<div class="mainContentItem full">
+	        <span class="errorCodeTitle">404 error</span>
+	        <span class="standardDividerLine"></span>
+	        <p class="marginTop paddingTop">
+	        	The page you were looking for could not be found.
+	        </p>
+        </div>
+    </div>
+</div>
