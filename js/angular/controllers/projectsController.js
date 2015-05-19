80d941a939f2d1f785f661d10a3ee153c1d95d75
Martin Sonesson <sonesson8909@hotmail.com>
2015-05-20 00:10:09 +0200

    fixed projects page

--- js/angular/controllers/projectsController.js
+++ js/angular/controllers/projectsController.js
@@ -40,85 +40,10 @@
             $(document).unbind();
         }
 
-        function setSizes() {
-
-            if ($scope.projects.length % 5 === 0)
-            {
-                return;
-            } else {
-                var numberOfProjectsOnLastRow = $scope.projects.length % 5;
-                var numberOfProjectsToShowLarge = 5 - numberOfProjectsOnLastRow;
-                var numberOfRows = Math.ceil($scope.projects.length / 5);
-
-                angular.forEach($scope.projects, function(project){
-                    project.showLarge = (false);
-                });
-
-                while (numberOfProjectsToShowLarge > 0)
-                {
-                    var randomIndex = Math.floor((Math.random() * $scope.projects.length) + 0);
-                    $scope.projects[randomIndex].showLarge = true;
-                    numberOfProjectsToShowLarge--;
-                }
-                //fixRowProblems();
-            }
-        }
-
-        function fixRowProblems(){
-            var minIndex = 0;
-            var maxIndex = 4;
-            var currentRowSize = 0;
-            for (var i = 0; i < $scope.projects.length; i++)
-            {
-                if (getTotalSize() % 5 !== 0)
-                {
-                    if ($scope.projects[i].showLarge)
-                    {
-                        currentRowSize += 2;
-                    } else {
-                        currentRowSize++;
-                    }
-
-                    if (currentRowSize === 0)
-                    {
-                        minIndex = i;
-                    }
-                    if (currentRowSize === 5)
-                    {
-                        maxIndex = i;
-                        currentRowSize = 0;
-                    }
-
-                    if (currentRowSize === 4 && $scope.projects[i+1] && $scope.projects[i+1].showLarge){
-                        var randomIndex = Math.floor((Math.random() * maxIndex) + minIndex);
-                        $scope.projects[randomIndex].showLarge = true;
-                    } else if (currentRowSize === 4 && !$scope.projects[i+1]){
-                        var randomIndex = Math.floor((Math.random() * i) + minIndex);
-                        $scope.projects[randomIndex].showLarge = true;
-                    }
-                }
-            }
-        }
-
-        function getTotalSize(){
-            var totalRowSize = 0;
-            for (var i = 0; i < $scope.projects.length; i++)
-            {
-                if ($scope.projects[i].showLarge)
-                {
-                    totalRowSize += 2;
-                } else {
-                    totalRowSize++;
-                }
-            }
-            return totalRowSize;
-        }
-
         function init() {
             $http.get('json/projects.json').success(function(data) {
                 //shuffle(data);
                 $scope.projects = data;
-                setSizes();
                 $log.debug($scope.projects);
             });
 
