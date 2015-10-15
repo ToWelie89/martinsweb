f8b7b749d617e16393c16c4170972fa9feda515f
ToWelie89 <sonesson8909@hotmail.com>
2015-10-15 14:29:09 +0200

    added instaanalytics

---
+++ includes/projects/instaanalytics.php
@@ -0,0 +1,29 @@
+<div id="backLink"><span>X</span></div>
+<div class="marginTop paddingTop">
+    <div class="table center paddingTop">
+        <div class="tableRow">
+            <div class="tableRowCell projectsInfoFirstColumn"></div>
+            <div class="tableRowCell projectsInfoSecondColumn">
+                <h5>Instagram Analyzer</h5>
+            </div>
+        </div>
+        <div class="tableRow">
+            <div class="tableRowCell projectsInfoFirstColumn">
+                <img src="img/projectHeaders/wcc.png" class="projectsInfoPicture">
+            </div>
+            <div class="tableRowCell projectsInfoSecondColumn">
+                <p class="bodyText">
+                    Write something here
+                </p>
+                <div class="paddingTop floatRight">
+                    <a class="button" href="https://github.com/ToWelie89/InstagramAnalytics" target="_blank">
+                        <p text="Code"></p>
+                    </a>
+                    <a class="button" href="http://martinsonesson.se/pi/instaanalytics/" target="_blank">
+                        <p text="Demo"></p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
