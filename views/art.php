e84f09b2bba3a0cbd37e221a46c8def8ea818fb4
Martin Sonesson <sonesson8909@hotmail.com>
2016-05-20 02:00:53 +0200

    minor fixes

--- views/art.php
+++ views/art.php
@@ -3,7 +3,7 @@
         <p class="standardText">
             <div data-ng-include src="'includes/loadBox.html'" data-ng-show="loading"></div>
             <div data-ng-show="!loading">
-                <img class="artGalleryImg" src="{{ artItem.video ? '../../assets/build/play_icon.png' : '' }}"
+                <img class="artGalleryImg" src="{{ artItem.video ? '../../assets/build/play_icon.png' : '../../assets/build/blank-overlay.png' }}" width="150" height="150"
                  style="background-image: url({{artItem.thumbnail.url}});" data-ng-click="openPhotoSwipe(artItem)"
                  data-ng-repeat="artItem in art"/>
             </div>
