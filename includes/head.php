f8b28a1238456bdda5ddef4b9eca0f998cbb628d
ToWelie89 <sonesson8909@hotmail.com>
2016-10-05 14:51:01 +0200

    minor changes

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,4 @@
-<link rel="shortcut icon" href="img/icon.ico">
+<link rel="shortcut icon" href="assets/img/icon.ico">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta charset="utf-8"/>
