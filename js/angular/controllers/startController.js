b9d033b89071cb43e4fc5a29a294b6c6114b2afc
ToWelie89 <sonesson8909@hotmail.com>
2016-12-15 16:36:57 +0100

    minor fix

--- js/angular/controllers/startController.js
+++ js/angular/controllers/startController.js
@@ -16,46 +16,27 @@
          * @description Initilization function
          */
         function init() {
-            $('.signature svg').each(function() {
-                var delay;
-                var i;
-                var len;
-                var length;
-                var path;
-                var paths;
-                var previousStrokeLength;
-                var results;
-                var speed;
-                paths = $('path, circle, rect', this);
-                delay = 0;
-                results = [];
+            var delay;
+            var length;
+            var paths;
+            var previousStrokeLength;
+            var speed;
+            paths = $('path, circle, rect');
+            delay = 0;
 
-                for (i = 0, len = paths.length; i < len; i++) {
-                    path = paths[i];
-                    length = path.getTotalLength();
-                    previousStrokeLength = speed || 0;
-                    speed = length < 100 ? 20 : Math.floor(length);
-                    delay += previousStrokeLength + 100;
-                    results.push($(path).css('transition', 'none').attr('data-length', length).attr('data-speed', speed).attr('data-delay', delay).attr('stroke-dashoffset', length).attr('stroke-dasharray', length + ',' + length));
-                }
+            paths.each(function() {
+                length = $(this).get(0).getTotalLength();
+                previousStrokeLength = speed || 0;
+                speed = length < 100 ? 20 : Math.floor(length);
+                delay += previousStrokeLength + 100;
+                $(this).css('transition', 'none').attr('data-length', length).attr('data-speed', speed).attr('data-delay', delay).attr('stroke-dashoffset', length).attr('stroke-dasharray', length + ',' + length);
             });
-            $('.signature svg').each(function() {
-                var delay;
-                var i;
-                var len;
-                var path;
-                var paths;
-                var results;
-                var speed;
-                paths = $('path, circle, rect', this);
-                results = [];
-                for (i = 0, len = paths.length; i < len; i++) {
-                    path = paths[i];
-                    length = $(path).attr('data-length');
-                    speed = $(path).attr('data-speed');
-                    delay = $(path).attr('data-delay');
-                    results.push($(path).css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear').attr('stroke-dashoffset', '0'));
-                }
+
+            paths.each(function() {
+                length = $(this).attr('data-length');
+                speed = $(this).attr('data-speed');
+                delay = $(this).attr('data-delay');
+                $(this).css('transition', 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear').attr('stroke-dashoffset', '0');
             });
         }
 
